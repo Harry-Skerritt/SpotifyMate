25dafb24ccb6418f0cac2091f7b1bcb50898733f
Harry Skerritt <harry2.skerritt@live.uwe.ac.uk>
2024-12-30 01:15:02 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+//Check if network creds
+//If so attempt connection
+//If not lauch captive portal
+//Get Creds
+//Loop
+
+//On start display splash screen and loading whilst checking network creds and/or attempting connection
+//If captive displayed then show how to use on the screen
+//Display network connecting
+//Go to home (temp for now)
+
+//On Very Very First Launch
+//Generate a settings file
+//Generate a random 5 digit number - the device identifier
+//Put that identifier in the settings in the form of a name
+//This is also where the wifi credentials will be stored
+
+
+void start() {
+
+}
+
+void loop() {
+
+}
